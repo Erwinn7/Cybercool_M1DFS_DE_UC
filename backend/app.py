7b022d9d8721fcbbb698c417d587e4e7c1191233
ThomasSanna <thomassanna1e@gmail.com>
2025-11-20 10:46:10 +0100

    ebauche très simple server

--- backend/app.py
+++ backend/app.py
@@ -9,9 +9,16 @@ import uvicorn
 # App metadata
 app = FastAPI()
 
+@app.post("/login")
+async def login(request: Request):
+    data = await request.json()
+    username = data.get("username")
+    password = data.get("password")
+    print(f"Received login attempt for user: {username}, {password}")
+    return {"message": "Login successful"}
+
 
 # Run with: python app.py
 if __name__ == "__main__":
-
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=port, reload=True)
