4e8130b2081c70def16c7fef679000274a894092
ThomasSanna <thomassanna1e@gmail.com>
2026-01-14 10:21:50 +0100

    redirection attention.html

--- frontend/custom.js
+++ frontend/custom.js
@@ -30,12 +30,12 @@ document.addEventListener('DOMContentLoaded', function() {
                         body: formData
                     });
                     // const data = await response.json();
+                    window.location.href = 'attention.html'
                 } catch (error) {
                 }
             }
             
            
-            // window.location.href = '/'
         });
     }
 });
